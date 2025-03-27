1da960dbe184b5c7607f2479da5cd0b9b0022121
roger.balcells@outlook.com <Rbs612010@>
2025-03-26 20:38:32 -0400

    Version 1 documentation fix

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+# docs/conf.py
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Your Project Name'
+author = 'Your Name'
+release = '0.1.0'
+
+extensions = [
+    # list your Sphinx extensions here, e.g., 'sphinx.ext.autodoc'
+]
+
+html_theme = "sphinx_rtd_theme"
