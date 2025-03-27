903635dbdaedf011c24bdd512dce790b429ddbcf
roger.balcells@outlook.com <Rbs612010@>
2025-03-26 21:48:25 -0400

    Version 1 documentation fix

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'somnopy'
+copyright = '2025, Roger Balcells Sanchez, Thea Ng, Atif Abedeen, Lindsey Mooney'
+author = 'Roger Balcells Sanchez, Thea Ng, Atif Abedeen, Lindsey Mooney'
+release = '0.0.8'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
